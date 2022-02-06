225430505500f7edd2e7c93b8a0df6739464280c
Jaden Sierra <JadenSierra31@gmail.com>
2022-02-05 23:15:36 -0800

    Initial_commit

---
+++ First_Java_Program/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        System.out.println(" My name is Jaden \n I am 27 years old \n My hometown is Sunnyvale, CA");
+    }
+}
